1cec7f4ecece44828ae8e33a62ff98c634fc2d97
fukan <fukan@baijia.com>
2025-07-28 17:36:24 +0800

    husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,43 @@
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
+import globals from 'globals'
 
 export default [
+  {
+    ignores: [
+      '.turbo',
+      'node_modules',
+      'dist',
+      'build',
+      'coverage',
+      '.env',
+      '*.md',
+      '*.json',
+      '*.css',
+    ],
+  },
   js.configs.recommended,
   ...tseslint.config({
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: (await import('@typescript-eslint/parser')).default,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        project: [
+          './apps/dashboard/tsconfig.node.json',
+          './apps/dashboard/tsconfig.app.json',
+        ],
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      // 你可以在这里添加自定义规则
+    },
   }),
   prettier,
 ]
