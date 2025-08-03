98fb20cb6ea970d707883d2f79fcb8e7bc719f44
KevinFu <fukan211@gmail.com>
2025-08-03 08:22:48 +0800

    feat: add user info

--- apps/dashboard/src/components/UserInfo.tsx
+++ apps/dashboard/src/components/UserInfo.tsx
@@ -1,9 +1,37 @@
 import type { FC } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { Button, message } from 'antd'
+
+import { useRequest } from 'ahooks'
+import { getUserInfoService } from '../services/user'
 import { LOGIN_PATHNAME } from '../routers'
+import { UserOutlined } from '@ant-design/icons'
+import { removeUserToken } from '../utils/token'
 
 const UserInfo: FC = () => {
-  return (
+  const nav = useNavigate()
+
+  const { data = {} } = useRequest(getUserInfoService)
+
+  const { nickname } = data
+
+  function logout() {
+    removeUserToken()
+    message.info('LogOut successful')
+    nav(LOGIN_PATHNAME)
+  }
+
+  const UserInfo = (
+    <div className="text-cyan-100">
+      <UserOutlined />
+      {nickname}
+      <Button type="link" className="text-cyan-100" onClick={logout}>
+        LogOut
+      </Button>
+    </div>
+  )
+
+  const Login = (
     <Link
       to={LOGIN_PATHNAME}
       className="text-white hover:text-gray-300 transition-colors"
@@ -11,6 +39,8 @@ const UserInfo: FC = () => {
       Login
     </Link>
   )
+
+  return <>{nickname ? UserInfo : Login}</>
 }
 
 export default UserInfo
