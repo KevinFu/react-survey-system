1037d04ab0d38d51778f1b19b04a60204259ff06
KevinFu <fukan211@gmail.com>
2025-08-07 21:15:49 +0800

    chore: init portal project with Next.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,7 @@ export default [
   {
     ignores: [
       '**/.turbo/**',
+      '**/.next/**',
       '**/node_modules/**',
       '**/dist/**',
       '**/build/**',
@@ -47,6 +48,14 @@ export default [
         require: 'readonly',
         __dirname: 'readonly',
         __filename: 'readonly',
+      },
+    },
+  },
+  {
+    files: ['apps/portal/**/*.ts', 'apps/portal/**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: ['apps/portal/tsconfig.json'],
       },
     },
   },
